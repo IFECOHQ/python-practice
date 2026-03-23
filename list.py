e7f34107775000e1cf83dc0d3ae478dac0cfcfd6
IFECOHQ <ks07045299555@gmail.com>
2026-03-23 16:49:59 +0100

    lists

--- list.py
+++ list.py
@@ -1,3 +1,11 @@
 names = ['promise', 'ejiro', 'samuel']
 print(names[-2])
 print(names[1].title())
+message = f"my name is {names[1].title()}"
+print(message)
+names[1] = "ifeanyi"
+print(names)
+names.append("kingsley")
+print(names)
+names.insert[1, "ifeco"]
+print(names)
