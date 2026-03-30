6ac74970cd9812b262e9f3530cda4eb95856fe1c
IFECOHQ <ks07045299555@gmail.com>
2026-03-30 23:32:10 +0100

    newwork

--- workingwithlists.py
+++ workingwithlists.py
@@ -6,3 +6,16 @@ for magician in magicians:
 print("thank you, everyone that was a great show!") 
 for value in range(1, 5):
  print(value)
+numbers = list(range(1, 6))
+print(numbers)
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+squares = []
+for value in range(1, 12):
+ square = value ** 2
+ squares.append(square)
+print(squares)
+multiples = []
+for value in range(2, 10):
+ multiples.append(value ** 2) 
+print(multiples)
