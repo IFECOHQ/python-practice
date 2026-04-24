2f6a58b545f3b41dcada5b8f0fc5db3757a402dd
IFECOHQ <ks07045299555@gmail.com>
2026-04-24 18:42:36 +0100

    added an exercise

--- fillingadictionwithuserinput.py
+++ fillingadictionwithuserinput.py
@@ -13,5 +13,17 @@ while polling_active:
 # polling is complete. show the results.
 print('\n--- poll results ---')
 for name, response in responses.items():
-    print(f'{name} would like to climb {response}')        
+    print(f'{name} would like to climb {response}')
+
+# this is an exercise
+sandwich_orders = ['beans', 'rice', 'fiofio']
+finished_sandwiches = []
+while sandwich_orders:
+    current_orders = sandwich_orders.pop()
+    print(f"verifying users: {current_orders.title()}")  
+    finished_sandwiches.append(current_orders) 
+    print("the following users has been verified")
+    for finished_sandwich in finished_sandwiches:
+        print(finished_sandwich.title())
+print(finished_sandwiches)           
 
