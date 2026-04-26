25916e03d2c909ae6ebc1931285769982399bc72
IFECOHQ <ks07045299555@gmail.com>
2026-04-26 15:49:51 +0100

    Functions

---
+++ function.py
@@ -0,0 +1,10 @@
+def greet_user():
+    """Display a simple greeting."""
+    print("hello")
+greet_user()
+
+def respect_user(username):
+     """Display a simple greeting."""
+     print(f"hello, {username.title()}")
+respect_user('ejiro') 
+respect_user('adam')    
